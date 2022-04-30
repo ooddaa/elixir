d91c79d20c0917bf235b09f1756572b6a2ff0cbf
oda <ooddaa@gmail.com>
2022-04-30 20:35:53 +0100

    Lol tells jokes

--- onehour/mycode.ex
+++ onehour/mycode.ex
@@ -1,8 +1,25 @@
 defmodule MyModule do
   def engine do
     # something
-    name = IO.gets("what's up stranger, what's your name?") |> String.trim()
-    IO.puts("Hello #{name}")
+    # name = IO.gets("what's up stranger, what's your name? ") |> String.trim()
+    # IO.puts("Hello #{name}")
+
+    box = ["b", "c", a: "aaa"]
+    # # abc
+    # IO.puts(box)
+    IO.puts(box[:a])
+    do_stuff()
+  end
+
+  def do_stuff do
+    my_int = 987
+    my_str = "987"
+    IO.puts("Integer? #{is_integer(my_int)}")
+    # IO.puts("Integer? #{is_integer(my_str)} String? #{is_str(my_str)}")
+    IO.puts("Is atom? #{is_atom(:London)}")
+
+    range = 1..9
+    IO.puts(range[4])
   end
 end
 
