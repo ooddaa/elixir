d6ee0a1433938feab29ab39bc16919c0dbd51cbd
oda <ooddaa@gmail.com>
2022-05-04 21:09:02 +0100

    scoping of named functions -> use unquote to reach outer scopes vars

--- onehour/scope.ex
+++ onehour/scope.ex
@@ -6,17 +6,21 @@ defmodule One do
   you should be aware of the fact the a module definition creates the module as its side-effect, so the module itself will be available globally.
 
   https://elixir-lang.readthedocs.io/en/latest/technical/scoping.html
+
+  https://elixir-lang.readthedocs.io/en/latest/technical/scoping.html#named-functions-and-modules
+  First, defining a named function does not introduce a new binding into the current scope
+  Second, named functions cannot directly access surrounding scope, one has to use unquote to achieve that
   """
   # IO.puts(apple) # "apple"
 
   def main do
-    # IO.puts(apple) # var does not exist?
+    IO.puts(unquote(apple)) # need to unquote to access var from outer scope
 
-    one()
     two = fn ->
-      # IO.puts("two", apple) # no access to apple
-      IO.puts("two")
+      IO.puts(unquote(apple)) # same here
     end
+
+    # one() # ok
 
     # anonymous functions are not hoisted
     two.()
@@ -27,6 +31,8 @@ defmodule One do
     IO.puts("one")
     # IO.puts(apple) # var doesnt exist
   end
+
+  # main()  # not gonna work
 end
 
 One.main()
