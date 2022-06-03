3311aa8bdf0752886e9969c6675fa1340dadfae6
oda <ooddaa@gmail.com>
2022-06-03 12:14:58 +0100

    import IO

--- onehour/mycode.ex
+++ onehour/mycode.ex
@@ -1,54 +1,55 @@
 defmodule MyModule do
+  import IO
   @moduledoc """
   Well, quite frankly, the best module out there.
   """
 
   def engine do
     # something
-    # name = IO.gets("what's up stranger, what's your name? ") |> String.trim()
-    # IO.puts("Hello #{name}")
+    # name = gets("what's up stranger, what's your name? ") |> String.trim()
+    # puts("Hello #{name}")
 
     box = ["b", "c", a: "aaa"]
     # # abc
-    # IO.puts(box)
-    IO.puts(box[:a])
+    # puts(box)
+    puts(box[:a])
     do_stuff()
   end
 
   def do_stuff do
     my_int = 987
     # my_str = "987"
-    IO.puts("Integer? #{is_integer(my_int)}")
-    # IO.puts("Integer? #{is_integer(my_str)} String? #{is_string(my_str)}")
-    IO.puts("Is atom? #{is_atom(:London)}")
+    puts("Integer? #{is_integer(my_int)}")
+    # puts("Integer? #{is_integer(my_str)} String? #{is_string(my_str)}")
+    puts("Is atom? #{is_atom(:London)}")
 
     range = 1..9
-    IO.puts(range)
+    puts(range)
   end
 
   def data_stuff do
     # Integers
     my_int = 123
     int_check = if is_integer(my_int), do: "", else: "not"
-    IO.puts "#{my_int} is#{int_check} an integer"
+    puts "#{my_int} is#{int_check} an integer"
 
     # Floats
     my_float = 3.14
     float_check = if is_float(my_float), do: "", else: "not"
-    IO.puts "#{my_float} is#{float_check} a float"
+    puts "#{my_float} is#{float_check} a float"
 
     # Atoms
     my_atom = :Elixir
     atom_check = unless is_atom(my_atom), do: "not", else: ""
-    IO.puts "#{my_atom} is#{atom_check} an atom"
-    IO.puts "my_atom === :Elixir is #{my_atom === :Elixir}"
-    IO.puts ":\"Sleaford Mods\" == :\"Sleaford Mods\" is #{:"Sleaford Mods" == :"Sleaford Mods"}"
+    puts "#{my_atom} is#{atom_check} an atom"
+    puts "my_atom === :Elixir is #{my_atom === :Elixir}"
+    puts ":\"Sleaford Mods\" == :\"Sleaford Mods\" is #{:"Sleaford Mods" == :"Sleaford Mods"}"
 
     # Range
     range = 1..10
-    IO.puts "IO.puts 1 in range is #{1 in range}" # 2 in range |> IO.puts
+    puts "puts 1 in range is #{1 in range}" # 2 in range |> puts
 
-    Enum.each(1..3, &IO.puts/1)
+    Enum.each(1..3, &puts/1)
 
   end
 
@@ -56,7 +57,7 @@ defmodule MyModule do
     # if cond, do: this, else: that
     it_rains = true
     need_umbrella = if it_rains, do: "take it", else: "don't take it"
-    IO.puts need_umbrella
+    puts need_umbrella
   end
 
   def max(a,b) do
@@ -69,5 +70,5 @@ defmodule AnotherModule do
   # MyModule.engine()
   MyModule.data_stuff()
   # MyModule.control_flow()
-  # IO.puts MyModule.max(12, 43)
+  # puts MyModule.max(12, 43)
 end
