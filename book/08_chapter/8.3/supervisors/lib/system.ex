e2f01e4a2286a52dce0112accfd9a77df16ffb95
oda <ooddaa@gmail.com>
2022-10-06 20:51:20 +0100

    [8.3.5] using callback module

--- book/08_chapter/8.3/supervisors/lib/system.ex
+++ book/08_chapter/8.3/supervisors/lib/system.ex
@@ -1,7 +1,11 @@
 defmodule Todo.System do
+  use Supervisor
 
   def start_link do
-    Supervisor.start_link([Todo.Cache], strategy: :one_for_one)
+    Supervisor.start_link(__MODULE__, nil)
   end
 
+  def init(_) do
+    Supervisor.init([Todo.Cache], strategy: :one_for_one)
+  end
 end
