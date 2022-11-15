7381c6588c1eba1c74bc733eb8eb7eaf20212cf6
oda <ooddaa@gmail.com>
2022-11-15 10:31:08 +0000

    [10.3.4] SimpleRegistrty with spawned Agent

--- book/10_chapter/SimpleRegistry/SimpleRegistry.ex
+++ book/10_chapter/SimpleRegistry/SimpleRegistry.ex
@@ -1,48 +1,56 @@
 defmodule SimpleRegistry do
   use GenServer
 
-  def start_link(), do: GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  def start_link, do: GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  def register(name), do: GenServer.call(__MODULE__, {:register, name})
+  def whereis(name), do: GenServer.call(__MODULE__, {:whereis, name})
+  def state, do: GenServer.call(__MODULE__, {:state})
+
+  @impl GenServer
   def init(_) do
     Process.flag(:trap_exit, true)
     {:ok, %{}}
   end
 
-  def register(name), do: GenServer.call(__MODULE__, {:register, name})
-  def whereis(name), do: GenServer.call(__MODULE__, {:whereis, name})
-  def state, do: GenServer.call(__MODULE__, {:state})
-
-  def handle_call({:register, name}, _from, state) do
+  @impl GenServer
+  def handle_call({:register, name}, _, state) do
     case Map.has_key?(state, name) do
       true -> {:reply, :error, state}
       false ->
-        pid = case GenServer.start_link(__MODULE__, nil, name: name) do
-          {:ok, pid} -> pid
-          {:error, {:already_started, pid}} -> pid
-        end
-        new_state = Map.put(state, name, pid)
-        {:reply, :ok, new_state}
+        {:ok, pid} = Agent.start_link(fn -> %{} end)
+        IO.inspect("registering #{inspect pid}")
+        {:reply, :ok, Map.put(state, name, pid)}
     end
   end
 
-  def handle_call({:whereis, name}, _from, state) do
-    {:reply, Map.get(state, name, nil), state}
+  @impl GenServer
+  def handle_call({:whereis, name}, _, state) do
+    {:reply, Map.get(state, name), state}
   end
 
-  def handle_call({:state}, _from, state), do: {:reply, state, state}
+  @impl GenServer
+  def handle_call({:state}, _, state), do: {:reply, state, state}
 
+  @impl GenServer
   def handle_info(msg, state) do
     case msg do
       {:EXIT, pid, :killed} ->
-        IO.puts("process #{inspect pid} exited, de-registering")
-        new_state =
-          state
-          |> Enum.filter(fn { _, v } -> v !== pid end)
-          |> Map.new()
-        {:noreply, new_state}
+        IO.puts("process #{inspect pid} was killed, de-registering")
+        {:noreply, deregister(pid, state)}
+
+      {:EXIT, pid, :normal} ->
+        IO.puts("process #{inspect pid} exited as normal, de-registering")
+        {:noreply, deregister(pid, state)}
       _ ->
         IO.puts("handle_info received msg: #{inspect msg}")
         {:noreply, state}
     end
+  end
+
+  defp deregister(pid, state) do
+    state
+      |> Enum.filter(fn { _, v } -> v !== pid end)
+      |> Map.new()
   end
 
 end
